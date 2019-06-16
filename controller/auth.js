1484a8a11c8c3e4cfc1d63d320a483d7406de74a
Sai Kiran <sainik18@gmail.com>
2019-06-16 16:17:06 +0530

    Code dump

---
+++ controller/auth.js
@@ -0,0 +1,8 @@
+
+const auth = {
+    login : (req, res) => {
+        res.json({"data":"resp"});
+    }
+}
+
+module.exports = auth;
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// controller routes
+const auth = require('../controller/auth');
+
+router.post('/login', auth.login);
+
+module.exports = router;
