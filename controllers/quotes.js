f70edfa95578ff79bf51a0b7e0dd3e2b25d5f681
Sai Kiran <sainik18@gmail.com>
2019-06-22 08:24:36 +0530

    Working on language translation

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -4,17 +4,17 @@ const commonModel = require('../models/common.model');
 const quotes = {
     getQuotes: async(req, res) => {
 
-        // let text = 'hello i am john';
+        let text = 'hello i am john';
 
-        // let translate = await commonModel.translate(text, 'en-ru');
-        // res.json({data: translate});
+        let translate = await commonModel.translate(req.db, text, 'en-ru');
+        res.json({data: translate});
 
-        let quotes = await quotesModel.getQuotes(req.db);
-        if(quotes.length > 0){
-            res.json({status: true, data: quotes});
-        }else {
-            res.json({status: false, msg: 'No data found!'});
-        }
+        // let quotes = await quotesModel.getQuotes(req.db);
+        // if(quotes.length > 0){
+        //     res.json({status: true, data: quotes});
+        // }else {
+        //     res.json({status: false, msg: 'No data found!'});
+        // }
         
     }
 }
--- models/common.model.js
+++ models/common.model.js
@@ -24,20 +24,62 @@ const commonModel = {
             }
         });
     },
-    translate(text, lang) {
+    translate(conn, text, lang) {
         return new Promise(async (resolve, reject) => {
             try {
-                translate.translate(text, { to: lang }, function(err, res) {
-                    console.log(res);
-                    if(res.code == 200){
-                        resolve(res.text);
-                    }
-                    
-                  });
+                let data = await commonModel.getTranslation(conn, text, lang);
+                console.log(data, 334);
+                if(data){
+                    console.log("existing lang request");
+                    resolve(data);
+                }else{
+                    translate.translate(text, { to: lang }, function(err, res) {
+                        console.log(res);
+                        if(res.code == 200){
+                            console.log("new lang request");
+                            commonModel.insertTranslation(conn, text, lang, res.text[0]);
+                            resolve(res.text[0]);
+                        }
+                        
+                      });
+                }
+                
             } catch (error) {
                 reject(error);
             }
         })
+    },
+    getTranslation(conn, text, lang){
+        return new Promise(async (resolve, reject) => {
+            try {
+                let params = {
+                    lang: lang,
+                    eng: text 
+                };
+                let data = await conn.collection('translate').find(params).toArray();
+                if(data.length > 0){
+                    resolve(data[0].conText);
+                }else {
+                    resolve(false);
+                }
+
+            } catch (error) {
+                resolve(error);                
+            }
+        });
+    },
+    insertTranslation(conn, text, lang, conText){
+        try {
+            let params = {
+                lang: lang,
+                text: text,
+                conText: conText
+            }
+            let ins = conn.collection('translate').insertOne(params);
+            return true;
+        } catch (error) {
+            console.log(err, 122);
+        }
     }
 
 }
