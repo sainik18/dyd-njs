6d6b10b9e2fde24c679875ffaed86614420b5b53
Sai Kiran <sainik18@gmail.com>
2019-06-24 07:18:53 +0530

    Workin on login

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 var mongo = require('./middlewares/mongoConnection');
 
 var app = express();
+var cors = require('cors');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +18,7 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- controllers/quotes.js
+++ controllers/quotes.js
@@ -4,17 +4,19 @@ const commonModel = require('../models/common.model');
 const quotes = {
     getQuotes: async(req, res) => {
 
-        let text = 'hello i am john';
 
-        let translate = await commonModel.translate(req.db, text, 'en-ru');
-        res.json({data: translate});
 
-        // let quotes = await quotesModel.getQuotes(req.db);
-        // if(quotes.length > 0){
-        //     res.json({status: true, data: quotes});
-        // }else {
-        //     res.json({status: false, msg: 'No data found!'});
-        // }
+        // let translate = await commonModel.translate(req.db, text, 'en-ru');
+        // res.json({data: translate});
+        //let lang = req.body.lang;
+
+        let quotes = await quotesModel.getQuotes(req.db);
+
+        if(quotes.length > 0){
+            res.json({status: true, data: quotes});
+        }else {
+            res.json({status: false, msg: 'No data found!'});
+        }
         
     }
 }
