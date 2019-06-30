cf37c3e01b9a0f355240d3f04af69396a018ff93
Sai Kiran <sainik18@gmail.com>
2019-06-30 19:28:30 +0530

    Update devotions.js

--- controllers/devotions.js
+++ controllers/devotions.js
@@ -7,8 +7,26 @@ const devotions = {
         // let translate = await commonModel.translate(req.db, text, 'en-ru');
         // res.json({data: translate});
         //let lang = req.body.lang;
+        const { lang } = {...req.body};
 
-        let devotions = await devotionsModel.getDevotions(req.db);
+        let collection = 'devotions';
+        if(lang == 'Spanish'){
+            collection = 'devotionsSpanish';
+        }else if(lang == 'French'){
+            collection = 'devotionsFrench';
+        }else if(lang == 'Hausa'){
+            collection = 'devotionsHausa';
+        }else if(lang == 'Yoruba'){
+            collection = 'devotionsYoruba';
+        }else if(lang == 'Igbo'){
+            collection = 'devotionsIgbo';
+        }else if(lang == 'Portuguese'){
+            collection = 'devotionsPortuguese';
+        }else if(lang == 'German'){
+            collection = 'devotionsGerman';
+        }
+
+        let devotions = await devotionsModel.getDevotions(req.db, collection);
 
         if(devotions.length > 0){
             res.json({status: true, data: devotions});
