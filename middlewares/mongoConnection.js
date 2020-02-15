401aecbbefd43f90c09462c5c37074346f5707e3
sai kiran <sainik18@gmail.com>
2020-02-15 23:38:17 +0530

    Update mongoConnection.js

--- middlewares/mongoConnection.js
+++ middlewares/mongoConnection.js
@@ -1,9 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb://dydcoza:dydcoza%40123@ds155596.mlab.com:55596/dydcoza';
+//const url = 'mongodb://dydcoza:dydcoza%40123@ds155596.mlab.com:55596/dydcoza';
+//new one
+const url = 'mongodb+srv://dydcoza:dydcoza%40123@dyd-ogcns.mongodb.net/dydcoza';
+
 function mongoConn() {
     return new Promise((resolve, reject) => {
-        MongoClient.connect(url).then(client => {
+        MongoClient.connect(url, {useNewUrlParser: true }).then(client => {
             resolve(client.db());
         });
 
